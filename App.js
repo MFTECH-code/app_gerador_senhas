e3cb1c0b1b03a8564ec27ac7f30438256a2d9ec7
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-10-11 00:13:31 -0300

    Brincando com styled components

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import styled from "styled-components"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ViewStyle>
+      <Title>Gerador de senhas</Title>
+      <Button onPress={() => alert("Olá mundo!")}>
+        <TextButton>Gerar senha</TextButton>
+      </Button>
       <StatusBar style="auto" />
-    </View>
+    </ViewStyle>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const ViewStyle = styled.View`
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: #050A30;
+`
+
+const Title = styled.Text`
+  font-size: 40px;
+  font-weight: bold;
+  color: #fff;
+  margin: 15% 0;
+`
+
+const Button = styled.TouchableOpacity`
+  background-color: #fff;
+  border-radius: 17px;
+  
+`
+
+const TextButton = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: #050A30;
+  padding: 20px;
+`
+
