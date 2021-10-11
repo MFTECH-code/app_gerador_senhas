d8b09b9f260368d074dddd49f73da37882240505
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-10-11 01:44:48 -0300

    Gerador de senhas pronto

--- App.js
+++ App.js
@@ -1,15 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import styled from "styled-components"
 
+
 export default function App() {
+  const [senha, setSenha] = React.useState("")
+
+
+  const randint = (max) => {
+    return Math.floor((max) * Math.random() + 1)
+  }
+
+  const gerarSenha = (tamanhoSenha) => {
+    const letras = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQ"
+    const numeros = "1234567890"
+    const especiais = "!@#$%&*?"
+
+    const caracteres = [letras, numeros, especiais]
+
+    const senhaArray = []
+    
+    for (let i=0; i<tamanhoSenha; i++) {
+      let x = randint(caracteres.length - 1)
+      let y = randint(caracteres[x].length - 1)
+
+      senhaArray.push(caracteres[x][y])
+    }
+
+    let senhaString = ""
+
+    for (let i = 0; i < senhaArray.length; i++) {
+      senhaString += senhaArray[i]
+    }
+
+    setSenha(senhaString)
+  }
+  
+  const mostrarSenha = () => {
+    gerarSenha(8)
+  }
+
   return (
     <ViewStyle>
       <Title>Gerador de senhas</Title>
-      <Button onPress={() => alert("Olá mundo!")}>
+      <Button onPress={() => mostrarSenha()}>
         <TextButton>Gerar senha</TextButton>
       </Button>
+      <SenhaText>{senha}</SenhaText>
       <StatusBar style="auto" />
     </ViewStyle>
   );
@@ -21,6 +58,13 @@ const ViewStyle = styled.View`
   justify-content: flex-start;
   align-items: center;
   background-color: #050A30;
+`
+
+const SenhaText = styled.Text`
+  color: #7EC8E3;
+  font-size: 24px;
+  font-weight: bold;
+  margin-top: 40%;
 `
 
 const Title = styled.Text`
